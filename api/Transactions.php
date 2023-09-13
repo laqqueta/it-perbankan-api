35523caabb127d911bc251a74f68bccfd2c5ab66
je <envyxbot@gmail.com>
2023-09-13 20:16:18 +0700

    refactor code

--- api/Transactions.php
+++ api/Transactions.php
@@ -24,48 +24,48 @@ class Transactions
                 'message' => 'Internal Server Error.',
             ), JSON_PRETTY_PRINT);
             die();
-        } else {
-            $senderCurrentBalance = $result['result'][0]['b1'];
-            $receiverCurrentBalance = $result['result'][0]['b2'];
-
-            // check balance whether account balance is less than transfer amount or not
-            if ($senderCurrentBalance < $transferAmount) {
-                http_response_code(response_code: 200);
-                echo json_encode(array(
-                    'status' => 'warning',
-                    'message' => 'Account balance is less than transfer amount.',
-                ), JSON_PRETTY_PRINT);
-            } else {
-                $totalAmount = $receiverCurrentBalance + $transferAmount;
-                $currentBalance = $senderCurrentBalance - $transferAmount;
-                $query = "UPDATE user sender, user receiver SET sender.balance = $currentBalance, receiver.balance = $totalAmount WHERE sender.account_id = $fromAccount AND receiver.account_id = $toAccount";
-                $result = $this->db->executeUpdateQuery($query);
-
-                $date = date('Y-m-d');
-                $time = date('h:m:s');
-
-                echo $time;
-
-                if(is_null($result['result'])) {
-                    http_response_code(response_code: 500);
-                    echo json_encode(array(
-                        'status' => '500',
-                        'message' => 'Something went wrong.'
-                    ), JSON_PRETTY_PRINT);
-                } else {
-                    $this->updateTransactionHistory($fromAccount, $toAccount, $transferAmount, $date, $time);
-
-                    http_response_code(200);
-
-                    echo json_encode(array(
-                        'status' => $result['result'],
-                        'date' => $date,
-                        'time' => $time,
-                        'message' => 'Transfer to account ' . $toAccount . ' successful.'
-                    ), JSON_PRETTY_PRINT);
-                }
-            }
         }
+        
+        $senderCurrentBalance = $result['result'][0]['b1'];
+        $receiverCurrentBalance = $result['result'][0]['b2'];
+
+        // check balance whether account balance is less than transfer amount or not
+        if ($senderCurrentBalance < $transferAmount) {
+            http_response_code(response_code: 200);
+            echo json_encode(array(
+                'status' => 'warning',
+                'message' => 'Account balance is less than transfer amount.',
+            ), JSON_PRETTY_PRINT);
+            die();
+        }
+        
+        $totalAmount = $receiverCurrentBalance + $transferAmount;
+        $currentBalance = $senderCurrentBalance - $transferAmount;
+        $query = "UPDATE user sender, user receiver SET sender.balance = $currentBalance, receiver.balance = $totalAmount WHERE sender.account_id = $fromAccount AND receiver.account_id = $toAccount";
+        $result = $this->db->executeUpdateQuery($query);
+        
+        if(is_null($result['result'])) {
+            http_response_code(response_code: 500);
+            echo json_encode(array(
+                'status' => '500',
+                'message' => 'Something went wrong.'
+            ), JSON_PRETTY_PRINT);
+            die();
+        }
+
+        $date = date('Y-m-d');
+        $time = date('h:m:s');
+        
+        $this->updateTransactionHistory($fromAccount, $toAccount, $transferAmount, $date, $time);
+
+        http_response_code(200);
+
+        echo json_encode(array(
+            'status' => $result['result'],
+            'date' => $date,
+            'time' => $time,
+            'message' => 'Transfer to account ' . $toAccount . ' successful.'
+        ), JSON_PRETTY_PRINT);
     }
 
     // Function to retrieve transaction history for an account
